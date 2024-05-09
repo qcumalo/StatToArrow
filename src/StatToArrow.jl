484717ea5f2d300164c2af761a691a70f96cce16
qcumalo <141340427+qcumalo@users.noreply.github.com>
2024-05-09 16:19:10 +0200

    Update StatToArrow.jl

--- src/StatToArrow.jl
+++ src/StatToArrow.jl
@@ -1,7 +1,7 @@
 module StatToArrow 
 export statatoarrow
 using DataFrames, ReadStatTables, Arrow,ExportAll
-# Write your package code here.
+# Function to convert stata file to arrow file
     function statatoarrow(path_from::AbstractString,path_to::AbstractString)
     df = readstat(path_from) |>DataFrame
     Arrow.write(path_to, df)
